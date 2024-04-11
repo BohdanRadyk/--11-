540dc2325df77ec06e2710c1a1909126c8eb5e39
nazarzubykikh <nazar.zubyk.ikh.2023@lpnu.ua>
2024-04-11 19:08:57 +0300

    12.9

--- ConsoleApplication1/lab12.9.cpp
+++ ConsoleApplication1/lab12.9.cpp
@@ -2,7 +2,7 @@
 #include<conio.h>
 #include<math.h>
 #include<Windows.h>
-int main() // Otsinkamashynnohonulia
+int main()
 {
 	SetConsoleOutputCP(1251);
 	SetConsoleCP(1251);
@@ -14,6 +14,172 @@ m: precision = precision / 2.;
 	a = precision + 1.0;
 	i++;
 	if (a > 1.0) goto m;
-	printf("\n число ділень на 2: %6d\n", i);
-	printf("машинний нуль: %e\n ", precision);
-}
+	printf("GOTO");
+	printf("\nfloat число ділень на 2: %d  ", i);
+	printf("float машинний нуль: %e\n", precision);
+
+	int i_1 = 0;
+	double precision_1, a_1;
+	precision_1 = 1.0;
+m_1: precision_1 = precision_1 / 2.;
+	a_1 = precision_1 + 1.0;
+	i_1++;
+	if (a_1 > 1.0) goto m_1;
+	printf("double число ділень на 2: %d ", i_1);
+	printf("double машинний нуль: %le\n", precision_1);
+
+	int i_2 = 0; 
+	long double precision_2, a_2;
+	precision_2 = 1.0; 
+m_2: precision_2 = precision_2 / 2.;
+	a_2 = precision_2 + 1.0;
+	i_2++;
+	if (a_2 > 1.0) goto m_2;
+	printf("long double число ділень на 2: %d ", i_2);
+	printf("long double машинний нуль: %Le\n", precision_2);
+
+	//---------------------------
+		i = 0;
+		precision=0;
+		a=1.1;
+		precision = 1.0;
+		for (i; a > 1.0; i++)
+		{
+
+
+			precision = precision / 2.;
+			a = precision + 1.0;
+
+
+		}
+		printf("\n\nFOR");
+		printf("\nfloat число ділень на 2: %d ", i);
+		printf("floatмашинний нуль: %e\n", precision);
+
+		i_1 = 0;
+		precision_1 = 0;
+		a_1 = 1.1;
+		precision_1 = 1.0;
+		for (i_1; a_1 > 1.0; i_1++)
+		{
+
+
+			precision_1 = precision_1 / 2.;
+			a_1 = precision_1 + 1.0;
+
+
+		}
+		printf("double число ділень на 2: %d ", i_1);
+		printf("double машинний нуль: %le\n", precision_1);
+
+		i_2 = 0;
+		precision_2 = 0;
+		a_2 = 1.1;
+		precision_2 = 1.0;
+		for (i_1; a_2 > 1.0; i_2++)
+		{
+
+
+			precision_2 = precision_2 / 2.;
+			a_2 = precision_2 + 1.0;
+
+
+		}
+		printf("long double число ділень на 2: %d ", i_2);
+		printf("long double машинний нуль: %Le\n", precision_2);
+
+	//------------------------------
+		i = 0;
+		precision = 0;
+		a = 1.1;
+		precision = 1.0;
+		while (a > 1.0)
+		{
+			precision = precision / 2.;
+			a = precision + 1.0;
+			i++;
+		}
+		printf("\n\nWHILE");
+		printf("\nfloat число ділень на 2: %d ", i);
+		printf("floatмашинний нуль: %e\n", precision);
+
+
+		i_1 = 0;
+		precision_1 = 0;
+		a_1 = 1.1;
+		precision_1 = 1.0;
+		while (a_1 > 1.0)
+		{
+			precision_1 = precision_1 / 2.;
+			a_1 = precision_1 + 1.0;
+			i_1++;
+		}
+		printf("double число ділень на 2: %d ", i_1);
+		printf("double машинний нуль: %le\n", precision_1);
+
+
+		i_2 = 0;
+		precision_2 = 0;
+		a_2 = 1.1;
+		precision_2 = 1.0;
+		while (a_2 > 1.0)
+		{
+			precision_2 = precision_2 / 2.;
+			a_2 = precision_2 + 1.0;
+			i_2++;
+		}
+		printf("long double число ділень на 2: %d ", i_2);
+		printf("long double машинний нуль: %Le\n", precision_2);
+
+		//------------------------------
+		i = 0;
+		precision = 0;
+		a = 1.1;
+		precision = 1.0;
+		do
+		{
+			precision = precision / 2.;
+			a = precision + 1.0;
+			i++;
+		} while (a > 1.0);
+		{
+
+		}
+		printf("\n\nDO WHILE");
+		printf("\nfloat число ділень на 2: %d ", i);
+		printf("floatмашинний нуль: %e\n", precision);
+
+
+		i_1 = 0;
+		precision_1 = 0;
+		a_1 = 1.1;
+		precision_1 = 1.0;
+		do
+		{
+			precision_1 = precision_1 / 2.;
+			a_1 = precision_1 + 1.0;
+			i_1++;
+		} while (a_1 > 1.0);
+		{
+
+		}
+		printf("double число ділень на 2: %d ", i_1);
+		printf("double машинний нуль: %le\n", precision_1);
+
+
+		i_2 = 0;
+		precision_2 = 0;
+		a_2 = 1.1;
+		precision_2 = 1.0;
+		do
+		{
+			precision_2 = precision_2 / 2.;
+			a_2 = precision_2 + 1.0;
+			i_2++;
+		} while (a_2 > 1.0);
+		{
+
+		}
+		printf("long double число ділень на 2: %d ", i_2);
+		printf("long double машинний нуль: %Le\n", precision_2);
+	}
