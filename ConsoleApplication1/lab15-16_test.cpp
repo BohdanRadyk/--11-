c8700ee375878fbe72d5952044774679d4bae93b
nazarzubykikh <nazar.zubyk.ikh.2023@lpnu.ua>
2024-04-22 21:04:25 +0300

    Final 2

---
+++ ConsoleApplication1/lab15-16_test.cpp
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<time.h>
+#include <stdlib.h>
+
+void vuvid(int a[][2], int b)
+{
+	for (size_t i = 0; i < b; i++)
+	{
+		for (size_t j = 0; j < b; j++)
+		{
+			printf("%d", a[i][j]);
+		}
+		printf("\n");
+	}
+
+}
+
+
+int main()
+{
+	int const b = 2;
+	int riadok[b][b];
+	int counter = 1;
+	for (int i = 0; i < b; i++) {
+		for (int j = 0; j < b; j++) {
+			riadok[i][j] = counter++;
+		}
+	}
+	vuvid(riadok, b);
+}
